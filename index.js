10f2a5df1c9cba41a29193eb3d0ecfac9d4feaef
Spencer <yeshaopeng@lianqin360.com>
2017-06-16 18:12:04 +0800

    qrcode

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-(function(window){
+(function (window) {
     'use strict'
-    function onBrowserActionClick(tab){
-        var currentTab  = tab.getCurrent();
+    var qr = document.getElementById('qr');
+    var text = document.getElementById('text');
+    var qrcode = new QRCode(qr, {
+        height: 200,
+        width: 200
+    });
+    chrome.tabs.getSelected(function (tab) {
+        var url = '';
+        try {
+            url = tab.url
+        } catch (e) {
+        }
+        text.value = url;
+        string2QrCode(url);
+    })
+    document.getElementById('generate').addEventListener('click',function(){
+       string2QrCode(text.value) 
+    },false);
+    function string2QrCode(str) {
+        qrcode.makeCode(str);
     }
-
-    function string2QrCode(str){
-        
-    }
-    chrome.browserAction.onClicked.addListener(onBrowserActionClick);
 
 })(this);
